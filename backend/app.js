d52bec0e277e91ba985aefe8d6b03a9b693aee3f
sangnd2x <sanguyen128@gmail.com>
2022-10-27 15:48:55 +0700

    Asm1-basic adjust handling endpoint error commit

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,10 @@ app.use(movieRoute);
 
 // handling endpoint error
 app.use((req, res, next) => {
-    res.status(404).send('<h1>Page Not Found!</h1>');
+    res.statusCode = 401;
+    res.setHeader('Content-type', 'application/json');
+    res.write(JSON.stringify({ msg: 'Route not Found' }));
+    res.end();
 })
 
 app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
